2d08bc44761e67f524d40eae9136dc6cdc88e492
Ivan-Lopez08 <andreslomejia77@gmail.com>
2026-03-24 20:08:53 -0600

    W03 dynamic header and footer update 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,5 @@
 import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { ProductData } from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
